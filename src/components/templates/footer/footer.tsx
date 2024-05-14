4ba165159a1822f9b689559339cbf249e096bc71
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-05-14 21:51:50 +0600

    Update footer.tsx

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -14,12 +14,12 @@ export const Footer = () => {
         <div className="mt-8">
           {t('footer.powerBy')}{' '}
           <Link
-            href="https://www.contentful.com"
+            href="https://raselshikdar.is-a.dev"
             rel="noopener noreferrer"
             target="_blank"
             className="text-blue500"
           >
-            Contentful
+            Rasel Shikdar
           </Link>
         </div>
       </Container>
